873f0e5edd54c141354fd81dfb15802569e2d37f
ArchiDevil <archidevil52@gmail.com>
2014-12-07 19:30:02 +0300

    /W4, logger improvements

--- src/Utilities/logger.cpp
+++ src/Utilities/logger.cpp
@@ -23,65 +23,24 @@ Log::~Log()
 void Log::Message( const std::string & message )
 {
 #if defined (DEBUG) || (_DEBUG)
-	log << GetTimeString() << "INFO: " << message << std::endl;
-	std::cout << GetTimeString() << "INFO: " << message << std::endl;
+	log << "INFO: " << message << std::endl;
+	std::cout << "INFO: " << message << std::endl;
 #endif // DEBUG
 }
 
 void Log::Error( const std::string & error )
 {
-	log << GetTimeString() << "ERROR: " << error << std::endl;
-	std::cout << GetTimeString() << "ERROR: " << error << std::endl;
+	log << "ERROR: " << error << std::endl;
+	std::cout << "ERROR: " << error << std::endl;
 }
 
 void Log::FatalError( const std::string & error )
 {
-	log << GetTimeString() << "FATAL ERROR: " << error << std::endl;
-	std::cout << GetTimeString() << "FATAL ERROR: " << error << std::endl;
+	log << "FATAL ERROR: " << error << std::endl;
+	std::cout << "FATAL ERROR: " << error << std::endl;
 
 #if defined (WIN32) || (_WIN32)
 	::MessageBoxA(NULL, error.c_str(), "Fatal error!", MB_ICONERROR);
 #endif
 	throw std::runtime_error(error);
-}
-
-std::string Log::GetTimeString()
-{
-#if defined (WIN32) || (_WIN32)
-	SYSTEMTIME sm;
-	GetLocalTime(&sm);
-	std::ostringstream buffer;
-	buffer << "[["
-		<< sm.wDay
-		<< "."
-		<< sm.wMonth
-		<< "."
-		<< sm.wYear
-		<< "] "
-		<< sm.wHour
-		<< ":"
-		<< sm.wMinute
-		<< ":"
-		<< sm.wSecond
-		<< "]\t";
-	return buffer.str();
-#else
-    tm * time;
-    timelocal(time);
-	std::ostringstream buffer;
-	buffer << "[["
-		<< time->tm_mday
-		<< "."
-		<< time->tm_mon
-		<< "."
-		<< time->tm_year
-		<< "] "
-		<< time->tm_hour
-		<< ":"
-		<< time->tm_min
-		<< ":"
-		<< time->tm_sec
-		<< "]\t";
-	return buffer.str();
-#endif
 }
--- src/Utilities/logger.h
+++ src/Utilities/logger.h
@@ -3,24 +3,23 @@
 #include <string>
 #include <fstream>
 
-//#define LOG_FATAL_ERROR(fmt, ...)		MainLog.FatalError(__FILE__##":"##__LINE__##" "##fmt, ##__VA_ARGS__)
-//#define LOG_ERROR(fmt, ...)			MainLog.Error(__FILE__##":"##__LINE__##" "##fmt, ##__VA_ARGS__)
-//#define LOG_INFO(fmt, ...)			MainLog.Message(__FILE__##":"##__LINE__##" "##fmt, ##__VA_ARGS__)
-
 class Log
 {
 public:
 	Log(const std::string & FileName);
 	~Log();
 
-	void Message(const std::string & message );
-	void Error(const std::string & error );
-	void FatalError(const std::string & error );
+	void Message(const std::string & message);
+	void Error(const std::string & error);
+	void FatalError(const std::string & error);
 
 private:
-	static std::string GetTimeString();
 	std::ofstream log;
 
 };
 
 extern Log MainLog;
+
+#define LOG_FATAL_ERROR(str)			MainLog.FatalError(std::string(__FUNCTION__) + "():" + std::to_string(__LINE__) + " " + str)
+#define LOG_ERROR(str)					MainLog.Error(std::string(__FUNCTION__) + "():" + std::to_string(__LINE__) + " " + str)
+#define LOG_INFO(str)					MainLog.Message(std::string(__FUNCTION__) + "():" + std::to_string(__LINE__) + " " + str)
